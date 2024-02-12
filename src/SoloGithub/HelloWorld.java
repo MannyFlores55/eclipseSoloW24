6c9f9dcfdcb7de76863dcbe2424a29d3293d2a41
The-Doctorpus <manny.flores52@yahoo.com>
2024-02-12 14:08:12 -0500

    Updated so I could take a screenshot of the dialog box

--- src/SoloGithub/HelloWorld.java
+++ src/SoloGithub/HelloWorld.java
@@ -2,6 +2,6 @@ package SoloGithub;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Howdy github Project");
-		System.out.println("Alternate new words");
+		System.out.println("Alternate new word");
 	}
 }
