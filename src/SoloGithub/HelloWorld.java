cf42734fca1cdc2bcb8150b36d43f62415bf9f16
The-Doctorpus <manny.flores52@yahoo.com>
2024-02-12 10:49:17 -0500

    alternate next line

--- src/SoloGithub/HelloWorld.java
+++ src/SoloGithub/HelloWorld.java
@@ -2,6 +2,6 @@ package SoloGithub;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Howdy github Project");
-		System.out.println("Next line");
+		System.out.println("Alternate next line");
 	}
 }
